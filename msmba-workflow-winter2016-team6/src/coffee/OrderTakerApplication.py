6abdaa948fecdbd568d1b2ccf86d8aead2c6f190
luisapogo <luisapogo@gmail.com>
2016-01-07 09:57:09 -0500

    Update OrderTakerApplication.py

--- msmba-workflow-winter2016-team6/src/coffee/OrderTakerApplication.py
+++ msmba-workflow-winter2016-team6/src/coffee/OrderTakerApplication.py
@@ -4,7 +4,7 @@ The Order Taker User Interface for the coffee bar workflow (Winter Intensives La
 This is where you define the fields that appear on the screen (application) the order
 taker sees and tell WMP how this application (user interface) fits into the overall
 workflow.
-
+  
 Note:  the comments here assume you have already read through the comments
 in CoffeeBackend.py and made your edits there.
 '''
@@ -30,19 +30,21 @@ class OrderTakerApplication(RoleApplication):
         '''
         # Declare this application to be part of a given workflow, and responsible for a given role:
         # !!! Modify the following to use the actual role name you need...
-        super(OrderTakerApplication, self).__init__(theflowname, "RoleName") 
+        super(OrderTakerApplication, self).__init__(theflowname, "OrderTaker") 
         # Declare any tasks that this role is able to perform:
         # !!! Modify to use actual name for this task...
-        self.register_source_step("TaskName", self.take_drink_order_form_creator) 
+        self.register_source_step("TakeOrder", self.take_drink_order_form_creator) 
 
     def take_drink_order_form_creator(self, stepname, form):
         '''
-        This method does the actual work of building the user interface.
+        This method does the act ual work of building the user interface.
         '''
         # !!! improve this text...
-        form.add_static_label('You are supposed to do something here, I think:') 
-        # !!! Add at least two fields here, along with any additional static labels you need...
-
+        form.add_static_label('Please enter order details') 
+        form.add_field(Type.SHORTSTRING, "CustomerName")
+        form.add_field(Type.CHOICE, "CoffeeorTea?", choices=['Coffee', 'Tea'])  
+        form.add_field(Type.SHORTSTRING, "DrinkType")
+        # !!!  Add at least two   fields  here, along with any additional static labels you need...
 if __name__ == '__main__':
     #starts up the OrderTakerApplication:
     app = OrderTakerApplication()
