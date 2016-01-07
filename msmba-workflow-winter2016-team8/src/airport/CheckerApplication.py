80572f362bd8fb006857c979b2eef1eea6f0c827
yangxiao <yangxiao.thu@168.122.9.195>
2016-01-07 16:30:28 -0500

    CheckerApplication-v1

--- msmba-workflow-winter2016-team8/src/airport/CheckerApplication.py
+++ msmba-workflow-winter2016-team8/src/airport/CheckerApplication.py
@@ -6,29 +6,22 @@ from frontend.roleApplication import RoleApplication
 from frontend.form import Type
 from airport.AirportConstants import theflowname
 
+
 class CheckerApplication(RoleApplication):
+    """ The Checker User Interface """
 
     def __init__(self):
-        '''
-        Declare this application to be part of a given work flow and specify its role in that workflow.
-        '''
-        # Declare this application to be part of a given workflow, and responsible for a given role:
-        # !!! Modify the following to use the actual role name you need...
-        super(CheckerApplication, self).__init__(theflowname, "checker") 
-        
-        # Declare any tasks that this role is able to perform:
-        # !!! Modify to use actual task name and name_fields:
-        self.register_sink_step("checkID", self.checks_information, name_fields=["sequence", "Name", "ID"])
+        super(CheckerApplication, self).__init__(theflowname, "Checker");
+        # self.register_transition_step("Examine", self.examine_form_creator, name_fields=["sequence", "FirstName", "LastName", "Birthday"]);
+        self.register_transition_step("IDfit?", self.does_ID_fit_form_creator, name_fields=["sequence", "Name", "ID"]
 
 
-    def checks_information(self, stepname, form):
-        '''
-        Defines the data entry form for the checker application.
-        Appears after checker selects a pending order.
-        '''
-        # !!! Use one or more fields from order to define label...
-        form.add_task_label(fields=["Name"]) 
-        # !!! Add any static labels or fields you want to include in this form...
+    def does_ID_fit_form_creator(self, stepname, form):
+        form.add_task_label(fields=["Name", "ID"]);
+        form.add_field(Type.BOOLEAN, "IDfit?");
+  
+  
+
 
 if __name__ == '__main__':
     #starts up the CheckerApplication:
