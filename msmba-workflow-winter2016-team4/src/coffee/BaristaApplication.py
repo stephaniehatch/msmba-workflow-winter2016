377403c62110bc657e9dc1d9657352c9bb8b27ed
cdilley <cdilley@bu.edu>
2016-01-07 09:58:51 -0500

    Update BaristaApplication.py

--- msmba-workflow-winter2016-team4/src/coffee/BaristaApplication.py
+++ msmba-workflow-winter2016-team4/src/coffee/BaristaApplication.py
@@ -2,9 +2,7 @@
 The Barista User Interface for the coffee bar workflow (Winter Intensives Lab 5)
 
 This is where you define the fields that appear on the screen (application) the barista
-sees and tell WMP how this application (user interface) fits into the overall workflow.
-
-Note:  the comments here assume you have already read through the comments
+sees and tell WMP how this application (user interface) fits into the overall workflow.Note:  the comments here assume you have already read through the comments
 in CoffeeBackend.py and OrderTakerApplication.py and made your edits there.
 '''
 
@@ -27,12 +25,11 @@ class BaristaApplication(RoleApplication):
         '''
         # Declare this application to be part of a given workflow, and responsible for a given role:
         # !!! Modify the following to use the actual role name you need...
-        super(BaristaApplication, self).__init__(theflowname, "RoleName") 
+        super(BaristaApplication, self).__init__(theflowname, "Barista") 
         
         # Declare any tasks that this role is able to perform:
         # !!! Modify to use actual task name and name_fields:
-        self.register_sink_step("TaskName", self.prepare_drink_form_creator, name_fields=["sequence", "FieldName1", "FieldName2"])
-
+        self.register_sink_step("PrepareDrink", self.prepare_drink_form_creator, name_fields=["sequence", "DrinkName", "CustomerName"])
 
     def prepare_drink_form_creator(self, stepname, form):
         '''
@@ -40,9 +37,9 @@ class BaristaApplication(RoleApplication):
         This form appears once the barista selects one of the pending orders from a list.
         '''
         # !!! Use one or more fields from order to define label...
-        form.add_task_label(fields=["FieldName1","FieldName2"]) 
+        form.add_task_label(fields=["DrinkName","CustomerName"]) 
         # !!! Add any static labels or fields you want to include in this form...
-
+       
 if __name__ == '__main__':
     #starts up the BaristaApplication:
     app = BaristaApplication()
