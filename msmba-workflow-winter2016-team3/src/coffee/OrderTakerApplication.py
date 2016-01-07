420f417e365ac2c233747436a562f8dcd2fa5efb
mefreeze <mefree@bu.edu>
2016-01-07 10:01:59 -0500

    Update OrderTakerApplication.py

--- msmba-workflow-winter2016-team3/src/coffee/OrderTakerApplication.py
+++ msmba-workflow-winter2016-team3/src/coffee/OrderTakerApplication.py
@@ -30,17 +30,19 @@ class OrderTakerApplication(RoleApplication):
         '''
         # Declare this application to be part of a given workflow, and responsible for a given role:
         # !!! Modify the following to use the actual role name you need...
-        super(OrderTakerApplication, self).__init__(theflowname, "RoleName") 
+        super(OrderTakerApplication, self).__init__(theflowname, "OrderTaker") 
         # Declare any tasks that this role is able to perform:
         # !!! Modify to use actual name for this task...
-        self.register_source_step("TaskName", self.take_drink_order_form_creator) 
+        self.register_source_step("Ordering", self.take_drink_order_form_creator) 
 
     def take_drink_order_form_creator(self, stepname, form):
         '''
-        This method does the actual work of building the user interface.
+        This method does the actual work of building the user interface. Update 
         '''
         # !!! improve this text...
-        form.add_static_label('You are supposed to do something here, I think:') 
+        form.add_field(Type.SHORTSTRING, "Customer Name")
+        form.add_field(Type.SHORTSTRING, "Drink Type")
+        form.add_field(Type.CHOICE, "Size", choices=['Tall', 'Grande', 'Venti'])
         # !!! Add at least two fields here, along with any additional static labels you need...
 
 if __name__ == '__main__':
