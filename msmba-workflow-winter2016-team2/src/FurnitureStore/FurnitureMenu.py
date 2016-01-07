ae5c473ee20a6f7f23e9aba1f7d678b1bb75f345
katherinebroderick <katherinebroderick@wireless1x-168-122-11-128.bu.edu>
2016-01-07 13:36:52 -0500

    updated FurnitureStore

---
+++ msmba-workflow-winter2016-team2/src/FurnitureStore/FurnitureMenu.py
@@ -0,0 +1,57 @@
+'''
+The Order Taker User Interface for the coffee bar workflow (Winter Intensives Lab 5)
+
+This is where you define the fields that appear on the screen (application) the order
+taker sees and tell WMP how this application (user interface) fits into the overall
+workflow.
+
+Note:  the comments here assume you have already read through the comments
+in CoffeeBackend.py and made your edits there.
+'''
+
+# these next few lines import some of the WMP functions we will
+# use in this file.
+from frontend.roleApplication import RoleApplication
+from frontend.form import Type
+from CoffeeConstants import theflowname
+
+class OrderTakerApplication(RoleApplication):
+    '''
+    The OrderTakerApplication "class" is a collection of the "methods" (functions) that 
+    define the elements of the order taker application.  
+    
+    An application will always include the method __init__ and at least one
+    method to define a form that the user of this application will use.
+    '''
+
+    def __init__(self):
+        '''
+        Declare this application to be part of a given work flow and specify its role in that workflow.
+        '''
+        # Declare this application to be part of a given workflow, and responsible for a given role:
+        # !!! Modify the following to use the actual role name you need...
+        super(OrderTakerApplication, self).__init__(theflowname, "OrderTaker") 
+        # Declare any tasks that this role is able to perform:
+        # !!! Modify to use actual name for this task...
+        self.register_source_step("TakeOrder", self.take_drink_order_form_creator) 
+
+    def take_drink_order_form_creator(self, stepname, form):
+        '''
+        This method does the actual work of building the user interface.
+        '''
+        # !!! improve this text...
+        form.add_static_label('Order Form: ')
+        form.add_static_label('Please enter order and customer name below. ')
+        form.add_field(Type.SHORTSTRING, "CustomerName") 
+        form.add_field(Type.CHOICE, "DrinkName", choices=['coffee', 'tea', 'latte']) 
+        form.add_field(Type.INTEGER,'Quantity: ') 
+        
+        
+        
+        # !!! Add at least two fields here, along with any additional static labels you need...
+
+if __name__ == '__main__':
+    #starts up the OrderTakerApplication:
+    app = OrderTakerApplication()
+    #Start interacting with the user:
+    app.MainLoop()
---
+++ msmba-workflow-winter2016-team2/src/FurnitureStore/FurnitureOrder.py
@@ -0,0 +1,50 @@
+'''
+The Barista User Interface for the coffee bar workflow (Winter Intensives Lab 5)
+
+This is where you define the fields that appear on the screen (application) the barista
+sees and tell WMP how this application (user interface) fits into the overall workflow.
+
+Note:  the comments here assume you have already read through the comments
+in CoffeeBackend.py and OrderTakerApplication.py and made your edits there.
+'''
+
+from frontend.roleApplication import RoleApplication
+from frontend.form import Type
+from CoffeeConstants import theflowname
+
+class BaristaApplication(RoleApplication):
+    '''
+    The BaristaApplication "class" is a collection of the "methods" (functions) that 
+    define the elements of the order taker application.  
+    
+    An application will always include the method __init__ and at least one
+    method to define a form that the user of this application will use.
+    '''
+
+    def __init__(self):
+        '''
+        Declare this application to be part of a given work flow and specify its role in that workflow.
+        '''
+        # Declare this application to be part of a given workflow, and responsible for a given role:
+        # !!! Modify the following to use the actual role name you need...
+        super(BaristaApplication, self).__init__(theflowname, "Barista") 
+        
+        # Declare any tasks that this role is able to perform:
+        # !!! Modify to use actual task name and name_fields:
+        self.register_sink_step("PrepareDrink", self.prepare_drink_form_creator, name_fields=["sequence", "CustomerName", "DrinkName", "Quantity"])
+
+
+    def prepare_drink_form_creator(self, stepname, form):
+        '''
+        Defines the data entry form for the barista application.
+        This form appears once the barista selects one of the pending orders from a list.
+        '''
+        # !!! Use one or more fields from order to define label...
+        form.add_task_label(fields=["CustomerName","DrinkName"]) 
+        # !!! Add any static labels or fields you want to include in this form...
+
+if __name__ == '__main__':
+    #starts up the BaristaApplication:
+    app = BaristaApplication()
+    #Start interacting with the user:
+    app.MainLoop()
