82ba360e7cc683f84568d47314b6d9ea1ded3b9e
ayellis <ayellis@users.noreply.github.com>
2016-01-07 14:49:15 -0500

    Checker file with updated references.

--- msmba-workflow-winter2016-team8/src/airport/CheckerApplication.py
+++ msmba-workflow-winter2016-team8/src/airport/CheckerApplication.py
@@ -1,21 +1,12 @@
 '''
 The Airport Check-in Desk Interface for the airport workflow
-This is where you define the fields that appear on the screen (application) the barista
-sees and tell WMP how this application (user interface) fits into the overall workflow.
 '''
 
 from frontend.roleApplication import RoleApplication
 from frontend.form import Type
 from airport.AirportConstants import theflowname
 
-class DeskApplication(RoleApplication):
-    '''
-    The BaristaApplication "class" is a collection of the "methods" (functions) that 
-    define the elements of the order taker application.  
-    
-    An application will always include the method __init__ and at least one
-    method to define a form that the user of this application will use.
-    '''
+class CheckerApplication(RoleApplication):
 
     def __init__(self):
         '''
@@ -23,24 +14,24 @@ class DeskApplication(RoleApplication):
         '''
         # Declare this application to be part of a given workflow, and responsible for a given role:
         # !!! Modify the following to use the actual role name you need...
-        super(CheckerApplication, self).__init__(theflowname, "Barista") 
+        super(CheckerApplication, self).__init__(theflowname, "checker") 
         
         # Declare any tasks that this role is able to perform:
         # !!! Modify to use actual task name and name_fields:
-        self.register_sink_step("PrepareDrink", self.prepare_drink_form_creator, name_fields=["sequence", "Drank"])
+        self.register_sink_step("checkID", self.checks_information, name_fields=["insertfield1", "insertfield2"])
 
 
-    def prepare_drink_form_creator(self, stepname, form):
+    def checks_information(self, stepname, form):
         '''
-        Defines the data entry form for the barista application.
-        This form appears once the barista selects one of the pending orders from a list.
+        Defines the data entry form for the checker application.
+        Appears after checker selects a pending order.
         '''
         # !!! Use one or more fields from order to define label...
         form.add_task_label(fields=["Name"]) 
         # !!! Add any static labels or fields you want to include in this form...
 
 if __name__ == '__main__':
-    #starts up the BaristaApplication:
-    app = BaristaApplication()
+    #starts up the CheckerApplication:
+    app = CheckerApplication()
     #Start interacting with the user:
     app.MainLoop()
